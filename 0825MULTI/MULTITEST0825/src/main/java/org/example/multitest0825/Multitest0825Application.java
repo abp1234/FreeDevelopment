b1e6fc26687852f9cd8b4afac64cb850d9de64df
나경준 <testtesttestyo200@gmail.com>
2024-08-25 21:40:39 +0900

    feat:MultiDeploy

---
+++ 0825MULTI/MULTITEST0825/src/main/java/org/example/multitest0825/Multitest0825Application.java
@@ -0,0 +1,25 @@
+package org.example.multitest0825;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class Multitest0825Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Multitest0825Application.class, args);
+	}
+
+}
+@RestController
+@CrossOrigin(origins = "*") 
+class HelloController{
+	@GetMapping("/api/hello")
+	public String hello(){
+		return "Hello from Spring Boot!";
+	}
+}
---
+++ 0825MULTI/MULTITEST0825/src/test/java/org/example/multitest0825/Multitest0825ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.multitest0825;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Multitest0825ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
